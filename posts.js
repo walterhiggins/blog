3ef5563e0835f6ba6c9cc9d5087889b9cd9550a1
Walter Higgins <walter.higgins@gmail.com>
2018-12-22 17:38:35 +0000

    reload posts on each fetch

--- posts.js
+++ posts.js
@@ -1,20 +1,17 @@
 var fs = require("fs");
 
-let all = fs.readdirSync("posts");
+module.exports = function() {
+  let db = {
+    byDate: {}
+  };
+  let all = fs.readdirSync("posts");
+  let posts = all.filter(file => file.match(/\.md$/));
+  posts.forEach(post => {
+    let contents = fs.readFileSync(`posts/${post}`, "utf8");
+    let pubDate = contents.match(/## ([0-9\/\:\ ]+)/g)[0].substring(3);
+    let title = contents.match(/# ([a-zA-Z0-9\-]+)/g)[0].substring(2);
+    db.byDate[pubDate] = { title, pubDate, contents };
+  });
 
-let posts = all.filter(file => file.match(/\.md$/));
-
-let db = {
-  byDate: {}
+  return db;
 };
-
-posts.forEach(post => {
-  let contents = fs.readFileSync(`posts/${post}`, "utf8");
-  console.info(post);
-  let pubDate = contents.match(/## ([0-9\/\:\ ]+)/g)[0].substring(3);
-  let title = contents.match(/# ([a-zA-Z0-9\-]+)/g)[0].substring(2);
-  console.log(pubDate);
-  db.byDate[pubDate] = { title, pubDate, contents };
-});
-
-module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var router = express.Router();
-let posts = require("../posts");
+let postsFn = require("../posts");
 let marked = require("marked");
 /* GET home page. */
 router.get("/", function(req, res) {
+  let posts = postsFn();
   res.render("index", { posts, marked });
 });
 router.get("/posts.js", function(req, res) {
+  let posts = postsFn();
   res.send(`let posts = ${JSON.stringify(posts)}`);
 });
 module.exports = router;
