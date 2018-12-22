be432a65988b542e240c4c4ef0362935987898f0
Walter Higgins <walter.higgins@gmail.com>
2018-12-22 17:22:49 +0000

    ddd

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ let marked = require("marked");
 router.get("/", function(req, res) {
   res.render("index", { posts, marked });
 });
-
+router.get("/posts.js", function(req, res) {
+  res.send(`let posts = ${JSON.stringify(posts)}`);
+});
 module.exports = router;
